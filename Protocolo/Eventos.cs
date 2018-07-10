f1e5436b47b5d60814fe078f9c942828ef96fa43
salesprendes <salesprendes@gmail.com>
2018-07-10 15:03:05 +0200

    .
    .

--- Protocolo/Eventos.cs
+++ Protocolo/Eventos.cs
@@ -4,10 +4,10 @@ namespace Bot_Dofus_1._29._1.Protocolo
 {
     class Eventos
     {
-        public delegate void Escuchar();
+        public delegate void Escuchando();
         public delegate void EscuchaFallida(Exception ex);
         public delegate void SocketAceptado(ProtocoloSocket socket);
-        public delegate void PaqueteEntrada(byte[] data);
+        public delegate void PaqueteEntrada(byte[] paquete);
         public delegate void Conectado();
         public delegate void ConexionFallida(Exception ex);
         public delegate void SocketCerrado();
--- Protocolo/ProtocoloSocket.cs
+++ Protocolo/ProtocoloSocket.cs
@@ -1,10 +1,10 @@
 ﻿using System;
+using System.Net;
 using System.Net.Sockets;
-using static Bot_Dofus_1._29._1.Protocolo.Eventos;
 
 namespace Bot_Dofus_1._29._1.Protocolo
 {
-    class ProtocoloSocket
+    class ProtocoloSocket : Eventos
     {
         private object bloqueo = new object();
         private byte[] buffer = new byte[3004];
@@ -17,13 +17,118 @@ namespace Bot_Dofus_1._29._1.Protocolo
 
         public ProtocoloSocket(Socket conexion, int tamano_buffer)
         {
+            buffer = new byte[tamano_buffer];
+            socket = conexion;
+            paquete_Recibido();
+        }
+
+        public void Conectar(string ip, int puerto)
+        {
             try
             {
-                buffer = new byte[tamano_buffer];
-                socket = conexion;
-                //ThreadReceived();
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.BeginConnect(IPAddress.Parse(ip), puerto, new AsyncCallback(conectar_CallBack), socket);
             }
-            catch (Exception){}
+            catch (Exception ex)
+            {
+                evento_Fallo_Conexion(ex);
+            }
+        }
+
+        public void paquete_Recibido()
+        {
+            socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(recibir_CallBack), socket);
+        }
+
+        public void enviar_Paquete(byte[] paquete)
+        {
+            lock (bloqueo)
+            {
+                socket.Send(paquete);
+            }
+        }
+
+        public void cerrar_Socket()
+        {
+            lock (bloqueo)
+            {
+                socket.Close();
+            }
+        }
+
+        private void conectar_CallBack(IAsyncResult ar)
+        {
+            try
+            {
+                if (socket.Connected)
+                {
+                    evento_Conexion_Conectado();
+                    paquete_Recibido();
+                }
+                else
+                    evento_Fallo_Conexion(new Exception("Not Connect"));
+            }
+            catch (Exception ex)
+            {
+                evento_Fallo_Conexion(ex);
+            }
+        }
+
+        private void recibir_CallBack(IAsyncResult ar)
+        {
+            lock (bloqueo)
+            {
+                int longitud = socket.EndReceive(ar);
+                if (longitud > 0)
+                {
+                    byte[] data = new byte[longitud];
+                    for (int i = 0; i <= longitud - 1; i++)
+                        data[i] = buffer[i];
+                    evento_Nuevo_Paquete(data);
+                    paquete_entrada_evento?.Invoke(data);
+                    buffer = new byte[5000];
+                    paquete_Recibido();
+                }
+                else
+                {
+                    evento_Socket_Cerrado();
+                }
+            }
+        }
+
+        public string IP
+        {
+            get
+            {
+                try
+                {
+                    return socket.RemoteEndPoint.ToString();
+                }
+                catch (Exception)
+                {
+                    return "null";
+                }
+            }
+        }
+
+        private void evento_Nuevo_Paquete(byte[] paquete)
+        {
+            paquete_entrada_evento?.Invoke(paquete);
+        }
+
+        private void evento_Socket_Cerrado()
+        {
+            socket_cerrado_evento?.Invoke();
+        }
+
+        private void evento_Fallo_Conexion(Exception ex)
+        {
+            conexion_Fallida_evento?.Invoke(ex);
+        }
+
+        private void evento_Conexion_Conectado()
+        {
+            conectado_evento?.Invoke();
         }
     }
 }
