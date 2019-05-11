af2f3dfb12d3c90308714e89cee41b976c5b30c3
salesprendes <salesprendes@gmail.com>
2019-05-11 23:45:07 +0200

    .

--- Otros/Mapas/Celda.cs
+++ Otros/Mapas/Celda.cs
@@ -7,6 +7,7 @@
 */
 
 using Bot_Dofus_1._29._1.Otros.Mapas.Interactivo;
+using Bot_Dofus_1._29._1.Protocolo.Enums;
 using System;
 
 namespace Bot_Dofus_1._29._1.Otros.Mapas
@@ -20,8 +21,9 @@ namespace Bot_Dofus_1._29._1.Otros.Mapas
         public byte layerGroundLevel { get; private set; } = 0;
         public byte layerGroundSlope { get; private set; } = 0;
         public ObjetoInteractivo objeto_interactivo { get; private set; }
+        public MapaTeleportCeldas tipo_teleport { get; private set; } = MapaTeleportCeldas.NINGUNO;
 
-        public Celda(short _id, TipoCelda _tipo, bool _es_linea_vision, byte _nivel, byte _slope, short _objeto_interactivo_id)
+        public Celda(short _id, TipoCelda _tipo, bool _es_linea_vision, byte _nivel, byte _slope, short _objeto_interactivo_id, Mapa _mapa)
         {
             id = _id;
             tipo = _tipo;
@@ -32,6 +34,23 @@ namespace Bot_Dofus_1._29._1.Otros.Mapas
 
             if (objeto_interactivo_id != -1)
                 objeto_interactivo = new ObjetoInteractivo(objeto_interactivo_id);
+
+            if(tipo == TipoCelda.CELDA_TELEPORT)
+            {
+                int temporal_x = _mapa.get_Celda_X_Coordenadas(id), temporal_y = _mapa.get_Celda_Y_Coordenadas(id);
+
+                if ((temporal_x - 1) == temporal_y)
+                    tipo_teleport = MapaTeleportCeldas.IZQUIERDA;
+                else if((temporal_x - 27) == temporal_y)
+                    tipo_teleport = MapaTeleportCeldas.DERECHA;
+                else if ((temporal_x + temporal_y) == 31)
+                    tipo_teleport = MapaTeleportCeldas.ABAJO;
+                else if (temporal_y < 0)
+                {
+                    if ((temporal_x - Math.Abs(temporal_y)) == 1)
+                        tipo_teleport = MapaTeleportCeldas.ARRIBA;
+                }
+            }
         }
     }
 }
