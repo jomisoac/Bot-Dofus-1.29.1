165d4d68ecf6fa1265aca3d6726e3fec090b6845
salesprendes <salesprendes@gmail.com>
2019-05-12 17:42:22 +0200

    celdas teleport!
    celdas teleport detectadas correctamente

--- Otros/Mapas/Celda.cs
+++ Otros/Mapas/Celda.cs
@@ -9,6 +9,7 @@
 using Bot_Dofus_1._29._1.Otros.Mapas.Interactivo;
 using Bot_Dofus_1._29._1.Protocolo.Enums;
 using System;
+using System.Linq;
 
 namespace Bot_Dofus_1._29._1.Otros.Mapas
 {
@@ -18,30 +19,39 @@ namespace Bot_Dofus_1._29._1.Otros.Mapas
         public short objeto_interactivo_id { get; private set; } = 0;
         public TipoCelda tipo { get; private set; } = TipoCelda.NO_CAMINABLE;
         public bool es_linea_vision { get; private set; } = false;
-        public byte layerGroundLevel { get; private set; } = 0;
-        public byte layerGroundSlope { get; private set; } = 0;
+        public byte layer_ground_nivel { get; private set; } = 0;
+        public byte layer_ground_slope { get; private set; } = 0;
         public ObjetoInteractivo objeto_interactivo { get; private set; }
         public MapaTeleportCeldas tipo_teleport { get; private set; } = MapaTeleportCeldas.NINGUNO;
 
-        public Celda(short _id, TipoCelda _tipo, bool _es_linea_vision, byte _nivel, byte _slope, short _objeto_interactivo_id, Mapa _mapa)
+        public static readonly int[] texturas_teleport = { 1030, 1029, 1764, 2298, 745 };
+
+        public Celda(short _id, TipoCelda _tipo, bool _es_linea_vision, byte _nivel, byte _slope, short _objeto_interactivo_id, short layer_object_1_num, short layer_object_2_num, Mapa _mapa)
         {
             id = _id;
-            tipo = _tipo;
+
+            if (texturas_teleport.Contains(layer_object_1_num) || texturas_teleport.Contains(layer_object_2_num))
+                tipo = TipoCelda.CELDA_TELEPORT;
+            else if (_tipo == TipoCelda.CELDA_TELEPORT)
+                tipo = TipoCelda.CELDA_CAMINABLE;
+            else
+                tipo = _tipo;
+
             es_linea_vision = _es_linea_vision;
-            layerGroundLevel = _nivel;
-            layerGroundSlope = _slope;
+            layer_ground_nivel = _nivel;
+            layer_ground_slope = _slope;
             objeto_interactivo_id = _objeto_interactivo_id;
 
             if (objeto_interactivo_id != -1)
                 objeto_interactivo = new ObjetoInteractivo(objeto_interactivo_id);
 
-            if(tipo == TipoCelda.CELDA_TELEPORT)
+            if (tipo == TipoCelda.CELDA_TELEPORT)
             {
                 int temporal_x = _mapa.get_Celda_X_Coordenadas(id), temporal_y = _mapa.get_Celda_Y_Coordenadas(id);
 
                 if ((temporal_x - 1) == temporal_y)
                     tipo_teleport = MapaTeleportCeldas.IZQUIERDA;
-                else if((temporal_x - 27) == temporal_y)
+                else if ((temporal_x - 27) == temporal_y)
                     tipo_teleport = MapaTeleportCeldas.DERECHA;
                 else if ((temporal_x + temporal_y) == 31)
                     tipo_teleport = MapaTeleportCeldas.ABAJO;
