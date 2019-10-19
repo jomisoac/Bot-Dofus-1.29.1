d2a9edb6aae1689e6b31f1722dab085a0262931d
Dylan <dyl.crel@gmail.com>
2019-10-19 10:07:54 +0200

    Update logique for find teleport cell

--- Utilidades/Extensiones/Extensiones.cs
+++ Utilidades/Extensiones/Extensiones.cs
@@ -60,7 +60,7 @@ namespace Bot_Dofus_1._29._1.Utilidades.Extensiones
 
         public static Dictionary<MapaTeleportCeldas, List<short>> Add(this Dictionary<MapaTeleportCeldas, List<short>> cells, short cellId)
         {
-            short[] topCells = new short[] { 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27 };
+            short[] topCells = new short[] { 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,36 };
             short[] rightCells = new short[] { 28, 57, 86, 115, 144, 173, 231, 202, 260, 289, 318, 347, 376, 405, 434 };
             short[] bottomCells = new short[] { 451, 452, 453, 454, 455, 456, 457, 458, 459, 460, 461, 462, 463 };
             short[] leftCells = new short[] { 15, 44, 73, 102, 131, 160, 189, 218, 247, 276, 305, 334, 363, 392, 421, 450 };
