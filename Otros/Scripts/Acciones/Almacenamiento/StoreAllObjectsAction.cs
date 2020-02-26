b134b7d5eea49835c16324baecbe7d576db46ad6
Dylan <dyl.crel@gmail.com>
2020-02-26 19:42:00 +0100

    Apply the hotfix for bank

--- Otros/Scripts/Acciones/Almacenamiento/StoreAllObjectsAction.cs
+++ Otros/Scripts/Acciones/Almacenamiento/StoreAllObjectsAction.cs
@@ -8,39 +8,34 @@ namespace Bot_Dofus_1._29._1.Otros.Scripts.Acciones.Almacenamiento
         internal override async Task<ResultadosAcciones> process(Account account)
         {
             Task[] tasks = new Task[account.hasGroup ? account.group.members.Count + 1 : 1];
-            tasks[0] = cleanInventory(account);
-
+            int idCapture = account.script.getCaptureIdAndQuantity().Key;
+            int idCAC = account.script.getCACId();
+            tasks[0] = cleanInventory(account, idCapture, idCAC);
             if (account.hasGroup && account.isGroupLeader)
             {
                 foreach (var follower in account.group.members)
                 {
-                    tasks[account.group.members.IndexOf(follower) + 1] = cleanInventory(follower);
+                    tasks[account.group.members.IndexOf(follower) + 1] = cleanInventory(follower, idCapture, idCAC);
                 }
             }
             Task.WaitAll(tasks);
             return ResultadosAcciones.HECHO;
         }
 
-        private async Task cleanInventory(Account account)
+        private async Task cleanInventory(Account account,int idcapture = 0, int idCAC = 0)
         {
             InventoryClass inventario = account.game.character.inventario;
 
-            int idCapture = account.script.getCaptureIdAndQuantity().Key;
-            int quantityCapture = account.script.getCaptureIdAndQuantity().Value;
+            
             foreach (InventoryObject objeto in inventario.objetos)
             {
-                if (!objeto.objeto_esta_equipado() && objeto.id_modelo != idCapture)
+                if (!objeto.objeto_esta_equipado() && idcapture != objeto.id_modelo && idCAC != objeto.id_modelo)
                 {
                     account.connexion.SendPacket($"EMO+{objeto.id_inventario}|{objeto.cantidad}");
                     inventario.eliminar_Objeto(objeto, 0, false);
                     await Task.Delay(300);
                 }
-                if(objeto.id_modelo == idCapture && objeto.cantidad< quantityCapture)
-                {
-                    int quantite = quantityCapture - objeto.cantidad;
-                    account.Logger.LogInfo("SCRIPT", $"On récupére " + quantite +" captures dans la banque");
-                    account.connexion.SendPacket($"EMO-{objeto.id_inventario}|{quantite}");
-                }
+
             }
         }
     }
